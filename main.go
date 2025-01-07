a8d4f30e1aed264ff22a3c03143bf09a2b6c1008
antikirra <antikirra@gmail.com>
2025-01-08 04:41:00 +0700

    UPD add .goreleaser.yml

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	i := flag.Bool("i", false, "Prompt the user to enter the password securely from the console (recommended)")
 	n := flag.Bool("pretty", false, "Ensure the resulting bcrypt hash does not contain special characters like dot or slash (recommended)")
 	t := flag.Bool("trim", false, "Do not add a newline at the end of the hash (useful for redirecting output)")
+	v := flag.String("verify", "", "The input hash value for verifying against the plaintext password")
 
 	flag.Parse()
 
@@ -41,7 +42,22 @@ func main() {
 		pass = *p
 	}
 
-	if pass == "" || *c > bcrypt.MaxCost || *c < bcrypt.MinCost {
+	if pass == "" {
+		printUsageAndExit()
+	}
+
+	if *v != "" {
+		err := bcrypt.CompareHashAndPassword([]byte(*v), []byte(pass))
+		if err == nil {
+			fmt.Println("Password verification succeeded")
+			os.Exit(0)
+		} else {
+			fmt.Println("Password verification failed")
+			os.Exit(1)
+		}
+	}
+
+	if *c > bcrypt.MaxCost || *c < bcrypt.MinCost {
 		printUsageAndExit()
 	}
 
